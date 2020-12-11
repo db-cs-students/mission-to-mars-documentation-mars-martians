56bb2d5e20dc23ee3b01401e184058c245c80265
Cmcclellan1738 <69971600+Cmcclellan1738@users.noreply.github.com>
2020-12-11 10:02:44 -0500

    Update Sender.py

--- Sender.py
+++ Sender.py
@@ -1,4 +1,7 @@
 radio.set_group(127)
 def on_forever():
+    led.toggle(4, 4)
     radio.send_value("x", input.acceleration(Dimension.X))
+    radio.send_value("y", input.acceleration(Dimension.Y))
+    radio.send_value("z", input.acceleration(Dimension.Z))
 basic.forever(on_forever)
